e949f1e4857f2000e4d0d3a551923f216d3c5eaf
AndreasDM <andreasmartila@gmail.com>
2018-11-15 16:37:05 +0100

    user classbllablab

--- Lottas_Flea_Market/User.cs
+++ Lottas_Flea_Market/User.cs
@@ -2,10 +2,25 @@ using System;
 
 namespace Lottas_Flea_Market {
 
-  class User {
-    // TODO: Create the fields for a user
-    // like name and credit.
-    // create a constructor
-  }
+  public class User {
+    public string name { set; get; }
+    public int capital { set; get; }
+    public int id      { set; get; }
+    
+    public User(int id, string name, int capital) {
+      this.id      = id;
+      this.name    = name;
+      this.capital = capital;
+    }
+    
+    // bid
+    public void Bid(string item) {
 
+    }
+    
+    // add bought items to list
+    public void AddBoughtItems() {
+
+    }
+  }
 }
